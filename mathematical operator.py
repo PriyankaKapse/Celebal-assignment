c6819cf68fe0350b82e1d0c784c1bdde9dbc8ab2
PriyankaKapse <100424142+PriyankaKapse@users.noreply.github.com>
2022-03-10 08:33:56 +0530

    Create mathematical operator.py

---
+++ mathematical operator.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a First Number: "))
+b=int(input("Enter a Second Number: "))
+c=a+b
+print("a+b =",c)
+
+d=a-b
+print("a-b =",d)
+
+e=a*b
+print("a*b =",e)
+
+f=a/b
+print("a/b =",f)
+
+g=a//b
+print("a//b =",g)
+
+h=a**b
+print("a**b =",h)
+
+i=a%b
+print("a%b =",i)
