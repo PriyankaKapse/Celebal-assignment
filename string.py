28aad7f39f0bc12736fc4c1b88948bf4339db3d5
PriyankaKapse <100424142+PriyankaKapse@users.noreply.github.com>
2022-03-10 08:40:36 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,20 @@
+
+mystring="Hello,I Am Priyanka"
+print(mystring)
+print(mystring[5])
+print(mystring[0])
+print(mystring[1])
+print(mystring[7])
+print(mystring[9])
+
+
+
+print(mystring[-7])
+print(mystring[-15])
+
+print(mystring[1:])
+print(mystring[1:5])
+print(mystring[11:18])
+print(mystring[11:])
+print(mystring[-3:-1])
+print(mystring[-8:-5])
