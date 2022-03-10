9e33bd480e7090734bb91b69b3abec50a9c974eb
PriyankaKapse <100424142+PriyankaKapse@users.noreply.github.com>
2022-03-10 08:34:33 +0530

    Create Comparison operator.py

---
+++ Comparison operator.py
@@ -0,0 +1,20 @@
+from multiprocessing import Condition
+a=int(input("Enter a First Number: "))
+b=int(input("Enter a Second Number: "))
+c=a>b
+print("a>b is",a>b)
+
+d=a<b
+print("a<b is",a<b)
+
+e=a==b
+print("a==b is ",a==b)
+
+f=a!=b
+print("a!=b is ",a!=b)
+
+e=a>=b
+print("a>=b is ",a>=b)
+
+f=a<=b
+print("a<=b is ",a<=b)
